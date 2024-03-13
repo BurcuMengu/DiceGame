a599ccb9b654f5cf7a147217da9776e68dc20b9a
Burcu Mengu <burcumengu@gmail.com>
2024-03-13 17:18:44 +0300

    push -u origin main

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ if (randomNumber1 > randomNumber2) {
     document.querySelector("h1").innerHTML = "🚩 Play 1 Wins!";
 }
 else if (randomNumber2 > randomNumber1) {
-    document.querySelector("h1").innerHTML = "🚩 Play 2 Wins!";
+    document.querySelector("h1").innerHTML = "Play 2 Wins! 🚩";
 }
 else {
     document.querySelector("h1").innerHTML = "Draw!";
